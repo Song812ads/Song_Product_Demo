16a85bea1a7f3b0155c0b6f8424f1ce6b9465237
Song812ads <songtran91105712@gmail.com>
2023-10-24 16:37:04 +0700

    dad

--- src/MainPage/App.js
+++ src/MainPage/App.js
@@ -33,8 +33,8 @@ function App() {
     client_secret: 'dgvVF4zg7mIa4NhK57mY7GOoNwxyGjUU',
     response_mode:'query',
     client_id: 'supply_client',
-    redirect_uri: 'https://song812ads.github.io/supply',
-    post_logout_redirect_uri:'https://song812ads.github.io/Song_Product_Demo/',
+    redirect_uri: 'https://song812ads.github.io/Song_Product_Demo/supply',
+    post_logout_redirect_uri:'https://song812ads.github.io/Song_Product_Demo/app',
     response_type: 'code',
     resource_access: {
         "song_supply": {
